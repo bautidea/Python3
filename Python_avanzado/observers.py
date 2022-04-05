5fbb9dd520998a3588d32eda140f2c724f621b9b
bautidea <juanbautistadeangelis@gmail.com>
2022-04-05 16:00:59 -0300

    added log.txt to Alta_observer

--- Python_avanzado/observers.py
+++ Python_avanzado/observers.py
@@ -1,3 +1,4 @@
+from ast import arg
 import os
 import datetime
 
@@ -9,18 +10,16 @@ class Tema():
         self.observadores.append(obj)
             
     def Notify_alta(self, *args):
-        self.observadores[0].Update(args)
+        self.observadores[0].Update(*args)
     
     def Notify_borrar(self, *args):
-        self.observadores[1].Update(args)
+        self.observadores[1].Update(*args)
     
     def Notify_modificar(self, *args):
-        self.observadores[2].Update(args)
+        self.observadores[2].Update(*args)
 
 class Log_observer():
-    ruta = os.path.dirname(os.path.abspath(__file__))+"\\observer_log.txt"
-    log = open(ruta, 'a+')
-    
+    ruta = os.path.dirname(os.path.abspath(__file__))+"\\observer_log.txt" 
     time = datetime.datetime.now().strftime("%d/%m/%y %H:%M")
 
 class Alta_observer(Log_observer):
@@ -29,8 +28,10 @@ class Alta_observer(Log_observer):
         self.obs_a.Add(self)
         
     def Update(self, *args):
-        print(args)
-
+        log = open(self.ruta, 'a+')
+        print(args[0])
+        print(self.time, '-', 'Titulo', args[0], 'creado', file= log)
+     
 class Borrar_observer(Log_observer):
     def __init__(self, obj):
         self.obs_b = obj
